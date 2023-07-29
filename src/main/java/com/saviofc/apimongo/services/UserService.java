e628bf8c563188ce16d941686a9699a4767f47d6
Savio <ferreirasavio39@gmail.com>
2023-07-29 14:47:37 -0300

    Obtendo um usuário por Id

--- src/main/java/com/saviofc/apimongo/services/UserService.java
+++ src/main/java/com/saviofc/apimongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.saviofc.apimongo.services;
 
 import com.saviofc.apimongo.domain.User;
 import com.saviofc.apimongo.repositories.UserRepository;
+import com.saviofc.apimongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,13 @@ public class UserService {
     public List<User> findAll(){
 
         return repository.findAll();
+    }
+
+    public User findById(String id){
+
+        Optional<User> userOptional = repository.findById(id);
+
+        return userOptional.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado para o ID: " + id));
+
     }
 }
